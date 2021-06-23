96830c4adc4df8dd1f41be8a5d841f4de139cf33
Shay Cohen <shay@pentagon2000.com>
2021-06-23 08:45:15 +0300

    Add project files.

---
+++ RecursiveStaircase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RecursiveStaircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
